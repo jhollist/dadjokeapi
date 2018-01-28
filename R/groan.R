7735f5a1bf409a6c9eec5b8a28bf9fd356b4b322
Jeffrey W. Holllister <jeff.w.hollister@gmail.com>
2018-01-28 08:32:31 -0500

    now with joke sting

--- R/groan.R
+++ R/groan.R
@@ -1,18 +1,18 @@
 #' Function to return random dad joke
 #'
 #' This function returns a random dad joke from \url{https://icanhazdadjoke.com}
-#' @param rimshot Plays a rimshot after the joke.  Sound from
+#' @param sting Plays a joke sting after the joke.  Sound from
 #'                \url{https://archive.org/details/Rimshot_254}
 #' @export
 #' @examples
 #' groan()
 
 
-groan <- function(rimshot = TRUE){
+groan <- function(sting = TRUE){
   request<-httr::GET("https://icanhazdadjoke.com",
                      httr::accept("text/plain"))
   joke <- httr::content(request, "text", encoding = "UTF-8")
-  if(rimshot){
+  if(sting){
     message(joke)
     Sys.sleep(0.5)
     beepr::beep(system.file("sounds/joke_sting.wav",package = "dadjoke"))
