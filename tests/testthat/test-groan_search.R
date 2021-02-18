f9b14cffb9b6c77994192f5623ff7f3f8a22f789
Jeff Hollister <jeff.w.hollister@gmail.com>
2021-02-17 20:46:45 -0500

    adding more to the test...

--- tests/testthat/test-groan_search.R
+++ tests/testthat/test-groan_search.R
@@ -1,10 +1,12 @@
 context("groan")
 skip_on_cran()
 x <- groan_search("cat")
+y <- groan_search("why")
 meowtain <- x$joke[x$id == "BQfaxsHBsrc"]
 
 test_that("groan gets a joke and an id", {
   expect_is(x, "tbl_df")
   expect_equal(names(x), c("joke", "id"))
   expect_equal(meowtain, "What do you call a pile of cats?  A Meowtain.")
+  expect_gt(nrow(y), 30)
 })
